c7d8cbc96f77d5a2e4262f2fd08af336d57a45f0
miablo <mvd5044@psu.edu>
2021-02-11 10:46:48 -0500

    Added assignment details

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * A function foo takes three integers as input arguments, i.e., foo(int a, int b, int c).
+ * The three inputs represent the three sides of a triangle in centimeter.
+ * The function is expected to return the type of a triangle: “equilateral”, “isosceles”, or “scalene”.
+ * Assume the domains of the three variables are 1 ≤ a ≤ 100, 50 ≤ b ≤ 150 and 100 ≤ c ≤ 200, respectively.
+ * A test case is in a tuple format <a, b, c, expected_output> with test inputs and the expected output.
+ *
+ * S1 is a set of test cases for the “Boundary Value Analysis” approach. (0.5 pt)
+ *
+ * S1 =
+
+ * S2 is a set of test cases for the “Robustness testing” approach. (0.5 pt)
+ *
+ * S2 – S1 =
+
+ * S3 is a set of tests cases for the “Robust Worst-Case testing” approach.
+ * Are there any types of triangles that S3 cannot reveal?
+ * If yes, what are they? If no, why? (1 pt)
+ *
+ */
+public class Main {
+
+}
